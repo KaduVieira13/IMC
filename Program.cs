1cec65f611c56cc0fecd6a3c8d9d60acdb00051f
KaduVieira13 <carloseduardo_alencar@outlook.com>
2021-03-22 10:37:55 -0300

    IMC concluido

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double alturaM;
+            double pesoKg;
+            double resultado;
+
+            Console.WriteLine("--- Cálculo de Índice de Massa Corporal ---");
+            Console.Write("Digite sua altura..: ");
+            alturaM = Convert.ToDouble(Console.ReadLine().Replace(".", ","));
+            Console.Write("Digite seu peso..: ");
+            pesoKg = Convert.ToDouble(Console.ReadLine());
+            resultado = pesoKg / Math.Pow(alturaM, 2);
+            Console.Write($"O IMC é..: {resultado:N1} Kg/m²"); 
+        }
+    }
+}
